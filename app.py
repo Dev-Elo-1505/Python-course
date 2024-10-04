463eedb488e7e9064807eccdeb2d847531e3bdc1
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2024-10-04 12:57:23 +0100

    numbers

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-job = "software engineer"
-print(job.format())
-print(job.join("joined"))
-print(job.center(200).strip())
-print(job.capitalize())
-print(job.isalnum())
+#numbers
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 3)
+print(10 // 3)
+print(10 ** 5)
+
+age = 10
+age /= 10
+print(age)
