0eb11c73315b36abfc55961b20931c079c5f9047
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2024-10-04 13:02:00 +0100

    working with numbers

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import math
 #numbers
-print(10 + 5)
-print(10 - 5)
-print(10 * 5)
-print(10 / 3)
-print(10 // 3)
-print(10 ** 5)
+# print(10 + 5)
+# print(10 - 5)
+# print(10 * 5)
+# print(10 / 3)
+# print(10 // 3)
+# print(10 ** 5)
 
 age = 10
 age /= 10
 print(age)
+print(round(2.6))
+print(math.sqrt(25))
